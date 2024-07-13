6a58136697cc5fecb03d140eff2d9dd1fa30fdb2
unknown <j.quabeck@gmx.net>
2024-07-13 12:26:25 +0200

    imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
+
 
 export const routes: Routes = [
-{path: '', component: LandingPageComponent}
+{path: '', component: LandingPageComponent},
+{path: 'LandingPage', component: LandingPageComponent},
+{path: 'Imprint', component: ImprintComponent}
 ];
